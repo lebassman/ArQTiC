adf850ac85b3ee307050b16b8dcce1bcacf28f0b
powerssc <cdpowers@usc.edu>
2020-08-06 11:52:06 -0700

    Updated run file
    Rigetti smart compiler appears to be re-causing the same problem that I just fixed with the normal rigetti compiler. Need to investigate more.

--- arqtic/run.py
+++ arqtic/run.py
@@ -3,21 +3,22 @@ tester=Heisenberg()
 
 
 
-# tester.backend="rigetti"
-# tester.auto_smart_compile="n"
-# tester.device_choice='8q-qvm'
-# tester.generate_circuits()
+tester.backend="rigetti"
+tester.device_choice='8q-qvm'
+tester.auto_smart_compile="n"
+tester.generate_circuits()
+tester.run_circuits()
 # tester.run_circuits()
-# # tester.run_circuits()
 
 
-# tester.backend="cirq"
-# tester.generate_circuits()
-# cirq_output=tester.return_circuits()
+tester.backend="cirq"
+tester.generate_circuits()
+cirq_output=tester.return_circuits()
+
 
 tester.backend="ibm"
 tester.device_choice="ibmq_16_melbourne"
-tester.compile="y"
+tester.auto_smart_compile="y"
 tester.noise_choice="n"
 tester.connect_account()
 tester.generate_circuits()
